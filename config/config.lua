bc1bf4147f1035b5c2388eae4c0e4cfbe34e93e2
Raggzinn <joaogabolivcar@gmail.com>
2023-07-06 22:54:11 -0300

    changes: REMOVENDO TODES AS BOSTAS
    Co-authored-by: Murilinho145SG <88172584+Murilinho145SG@users.noreply.github.com>

--- config/config.lua
+++ config/config.lua
@@ -1,15 +1,2 @@
--------------------------------------------
--- Configs da Hud
--------------------------------------------
-
 config_vida = 300
-config_logo = "https://cdn.discordapp.com/attachments/1120475800194523168/1122275621041614949/hud_logo.png"
 config_permstaff = "admin.permissao"
-
--------------------------------------------
--- Lista de Comados
--------------------------------------------
-
--- /cr (Limita Velocidade)
--- /eat (Regenera a Fome e Sede)
--- /hud (Tira a Hud da Tela)
